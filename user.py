b60114f05c30d762b9bd45ecb2a3f7265c67f694
Trev M <tbm50@georgetown.edu>
2025-05-24 18:54:44 -0600

    Worked on BlackJack.py

--- user.py
+++ user.py
@@ -3,12 +3,24 @@ class User:
     def __init__(self, numHands = 1, money = 1000):
         self.numHands = numHands
         self.money = money
-        self.hands = []
+        self.hands = {i: [] for i in range(numHands)}
     
-    def bet(self, bet):
+    def bet(self, bet, handIndex=0):
+        if handIndex < 0 or handIndex >= self.numHands:
+            raise IndexError("Invalid hand index")
+        
         if bet > self.money:
             raise ValueError("You don't have enough money to make this bet")
         self.money -= bet
         return bet
+    
+    def outcome(self, outcome, bet):
+        if outcome == "win":
+            self.money += bet * 2
+        elif outcome == "push":
+            self.money += bet
+        
+
+            
 
 
